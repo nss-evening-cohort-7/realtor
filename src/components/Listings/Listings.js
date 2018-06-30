211d223abe16ef65c73208e354bc95451b8b5a6a
Zoe Ames <zoeames@gmail.com>
2018-06-30 10:34:45 -0500

    added proptypes

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import {listingShape} from '../../propz/listingProp';
 import './Listings.css';
 
 class Listings extends React.Component {
+  static propTypes = {
+    listings: PropTypes.arrayOf(listingShape),
+  };
+
   render () {
     // const listings = this.props.listings;
     const {listings} = this.props;
---
+++ src/propz/listingProp.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+const listingShape = PropTypes.shape({
+  address: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+  zip: PropTypes.number.isRequired,
+  squareFootage: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  numBeds: PropTypes.number.isRequired,
+  numBaths: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  estimatedMonthlyMorgage: PropTypes.number.isRequired,
+  lotInAcres: PropTypes.number.isRequired,
+  yearBuilt: PropTypes.number.isRequired,
+  heating: PropTypes.string.isRequired,
+  cooling: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+});
+
+export {listingShape};
