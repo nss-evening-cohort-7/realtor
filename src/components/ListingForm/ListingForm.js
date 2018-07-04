dc71e03f8cf2a13e7ac9d5f269b7ff96ed09ed41
Zoe Ames <zoeames@gmail.com>
2018-07-03 21:03:30 -0500

    added proptypes

--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './ListingForm.css';
 
@@ -21,6 +22,10 @@ const defaultListing = {
 };
 
 class ListingForm extends React.Component {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  }
+
   state = {
     newListing: defaultListing,
   }
